ea17bc8204c8ee5035c4470a9c16e68799f7da34
Mojtaba Tajik <onyfel@gmail.com>
2024-12-02 11:45:50 +0100

    Add xunit test

--- src/TestProject/obj/Debug/net7.0/QodanaCoverageTest.AssemblyInfo.cs
+++ src/TestProject/obj/Debug/net7.0/QodanaCoverageTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QodanaCoverageTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e2c63233b44013297df573742072c6f36b1db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0510cb0409af9e852f53e4e00bd44c48331d62b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("QodanaCoverageTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QodanaCoverageTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ test/Project.Test/Class1.Tests.cs
@@ -0,0 +1,72 @@
+﻿using QodanaCoverageTest;
+
+namespace Project.Test
+{
+    public class Class1Tests
+    {
+        [Fact]
+        public void UsedMethod_ShouldExecuteWithoutException()
+        {
+            // Arrange
+            var class1 = new Class1();
+
+            // Act & Assert
+            class1.UsedMethod();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void UsedMethodWithIf_ShouldWriteToConsole(int input)
+        {
+            // Arrange
+            var class1 = new Class1();
+            using var consoleOutput = new ConsoleOutput();
+
+            // Act
+            class1.UsedMethodWithIf(input);
+
+            // Assert
+            var output = consoleOutput.GetOutput();
+            Assert.Contains(input.ToString(), output);
+        }
+
+        [Fact]
+        public void UsedLambdaMethod_ShouldReturn4()
+        {
+            // Arrange
+            var class1 = new Class1();
+
+            // Act
+            var result = class1.UsedLambdaMethod();
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+
+    // Helper class to capture console output
+    public class ConsoleOutput : IDisposable
+    {
+        private readonly StringWriter _stringWriter;
+        private readonly TextWriter _originalOutput;
+
+        public ConsoleOutput()
+        {
+            _stringWriter = new StringWriter();
+            _originalOutput = Console.Out;
+            Console.SetOut(_stringWriter);
+        }
+
+        public string GetOutput()
+        {
+            return _stringWriter.ToString();
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(_originalOutput);
+            _stringWriter.Dispose();
+        }
+    }
+}
